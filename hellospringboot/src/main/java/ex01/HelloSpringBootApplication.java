0e87de3004e40182d6255c601fe44a3710a1ba23
김현석 <gustjr324@gmail.com>
2022-06-21 12:28:49 +0900

    BootStrapping Class #1

---
+++ hellospringboot/src/main/java/ex01/HelloSpringBootApplication.java
@@ -0,0 +1,61 @@
+package ex01;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/*
+ * 1. 스프링부트 어플리케이션의  부트 스트래핑 역할(BootStrapping Class)
+ * 부트스트래핑(사물 초기단계에서 단순 요소로부터 복잡한 체계를 구축하는 과정) 클래스 
+ * 2. 설정 클래스로 역할(Configuration Class)
+ */
+
+@SpringBootApplication
+public class HelloSpringBootApplication {
+	
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return new MyComponent();
+	}	
+	public static void main(String[] args) {
+		
+		/*
+		 * SpringApplication.run(...) 안에서 일어나는 일(bootstrap)
+		 * 
+		 * 
+		 * 1. 어플리케이션 컨텍스트(Application Context, Spring Container) 생성
+		 * 2. 만약 웹 어플리케이션이면 웹어플리케이션의 타입을 결정(Spring MVC or Reactive)
+		 * 3. 어노테이셔 스캐닝(auto) + Configuration Class(Explicity) 통한 빈 생성/등록/DI/와이어링
+		 * 4. 웹 어플리케이션(MVC)
+		 * 		-- 내장(embedded) 서버(TomcatEmbeddedServletContainer)
+		 * 		-- 서버 인스턴스에 웹 어플리케이션 배포
+		 * 		-- 서버 인스턴스 실행
+		 * 5. ApplicationRunner 인터페이스를 구현한 빈을 찾아서 실행(run 호출)
+		 * 
+		 */
+//		MyComponent mc = ac.getBean(MyComponenet.class); 5번에서 이 역할을 해줌.
+//		System.out.println("do something1....");
+//		System.out.println("do something2....");
+//		System.out.println("do something3....");
+//		System.out.println("do something4....");
+		
+//		ConfigurableApplicationContext c = null;
+//		try {
+//		c = SpringApplication.run(HelloSpringBootApplication.class, args);
+//		} catch(Throwable ex) {
+//			ex.printStackTrace();
+//		} finally {
+//			c.close();
+//		}
+//		try ~ with ~ resource
+		try(ConfigurableApplicationContext c = SpringApplication.run(HelloSpringBootApplication.class, args)){
+			
+		}
+
+	}
+
+}
