e6002a5766ee0ab84c251c2fc6d2ab0ea68c9771
김현석 <gustjr324@gmail.com>
2022-06-22 10:25:46 +0900

    ..

--- springboot-myapplication/src/main/java/ex01/MyApplication.java
+++ springboot-myapplication/src/main/java/ex01/MyApplication.java
@@ -1,10 +1,20 @@
 package ex01;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MyApplication {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	@Bean
+	public MyComponent myComponent() {
+		return new MyComponent();
 	}
-
+	
+	public static void main(String[] args) {
+		try(ConfigurableApplicationContext c = SpringApplication.run(MyApplication.class, args)){}
+		
+	}
 }
