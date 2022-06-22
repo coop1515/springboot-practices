66d461e909352bb46a6e18fcda191d59ee481bd1
김현석 <gustjr324@gmail.com>
2022-06-22 12:12:36 +0900

    Myappliaction finish

--- hellospringboot/src/main/java/ex01/HelloSpringBootApplication.java
+++ hellospringboot/src/main/java/ex01/HelloSpringBootApplication.java
@@ -1,12 +1,8 @@
 package ex01;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 /*
  * 1. 스프링부트 어플리케이션의  부트 스트래핑 역할(BootStrapping Class)
@@ -17,10 +13,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class HelloSpringBootApplication {
 	
-	@Bean
-	public ApplicationRunner applicationRunner() {
-		return new MyComponent();
-	}	
 	public static void main(String[] args) {
 		
 		/*
---
+++ springboot-myapplication/src/main/java/ex05/component/MyComponent.java
@@ -0,0 +1,10 @@
+package ex05.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyComponent {
+	public void printHeloWorld() {
+		System.out.println("hello world");
+	}
+}
